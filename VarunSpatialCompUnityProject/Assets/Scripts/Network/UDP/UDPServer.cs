f7d911631a89a69a5202e61da07f51c90e16bcd6
brycedli <vgirdhar97@gmail.com>
2023-12-06 01:44:12 -0500

    UPDATE: UDP Server

--- VarunSpatialCompUnityProject/Assets/Scripts/Network/UDP/UDPServer.cs
+++ VarunSpatialCompUnityProject/Assets/Scripts/Network/UDP/UDPServer.cs
@@ -1,42 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading;
 using UnityEngine;
+
 
 public class UDPServer : MonoBehaviour
 {
-    [SerializeField]
-    string targetIPAddress = "172.26.98.224"; // Classroom computer's ipaddress
+    private UdpClient udpServer;
+    private IPEndPoint remoteEndPoint;
 
-    [SerializeField]
-    int portNumber = 5000;
-
-
-
-
-    IEnumerator Start()
+    private void Start()
     {
-        yield return new WaitForSeconds(1);
-        Send("test test test");
-    }
-    private void Send(string msg)
-    {
-        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-
-        IPAddress targetAddress = IPAddress.Parse(targetIPAddress);
-
-        IPEndPoint ep = new IPEndPoint(targetAddress, portNumber);
-
-
-        byte[] sendbuf = Encoding.ASCII.GetBytes(msg);
-
-        s.SendTo(sendbuf, ep); s.Close();
-
-        print("Message sent");
+        // Example: Start the UDP server on port 5555
+        StartUDPServer(5555);
     }
 
+    private void StartUDPServer(int port)
+    {
+        udpServer = new UdpClient(port);
+        remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
 
+        Debug.Log("Server started. Waiting for messages...");
+
+        // Start receiving data asynchronously
+        udpServer.BeginReceive(ReceiveData, null);
+    }
+
+    private void ReceiveData(IAsyncResult result)
+    {
+        byte[] receivedBytes = udpServer.EndReceive(result, ref remoteEndPoint);
+        string receivedMessage = System.Text.Encoding.UTF8.GetString(receivedBytes);
+
+        Debug.Log("Received from client: " + receivedMessage);
+
+        // Process the received data
+
+        // Continue receiving data asynchronously
+        udpServer.BeginReceive(ReceiveData, null);
+    }
+
+    private void SendData(string message, IPEndPoint endPoint)
+    {
+        byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(message);
+
+        // Send the message to the client
+        udpServer.Send(sendBytes, sendBytes.Length, endPoint);
+
+        Debug.Log("Sent to client: " + message);
+    }
 }
+
